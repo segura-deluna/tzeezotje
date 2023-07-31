dad73d9b84d1a48b86f5aabd3810f7030cc38c4b
Segura <segura-deluna@mail.ru>
2023-07-31 06:30:08 +0300

    start

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import { sum } from './modules/test.js'
+import { slider } from './modules/slider.js'
+
+console.log(sum(20, 10))
+
---
+++ src/js/modules/slider.js
@@ -0,0 +1,11 @@
+const slider = new Swiper('.slider', {
+  // Optional parameters
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
